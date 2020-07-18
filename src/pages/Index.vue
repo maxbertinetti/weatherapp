7f7438da9cc821c1859e3aaa53d15c5a145bf0e7
Massimiliano Bertinetti <acciaiodigitale@gmail.com>
2020-07-18 13:34:03 +0100

    :tada: First Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,98 @@
+<template>
+  <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input v-model="search" placeholder="Search" dark borderless>
+        <template v-slot:before>
+          <q-icon @click="getLocation" name="my_location" />
+        </template>
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
+
+    <template v-if="weatherData">
+      <div class="col text-white text-center">
+        <div class="text-h4 text-weight-light">
+          {{ weatherData.name }}
+        </div>
+        <div class="text-h6 text-weight-light">
+          {{ weatherData.weather[0].main }}
+        </div>
+        <div class="text-h1 text-weight-thin q-my-lg relative-position">
+          <span>{{ Math.round(weatherData.main.temp) }}</span>
+          <span class="text-h4 relative-position degree">&degC</span>
+        </div>
+      </div>
+
+      <div class="col text-center">
+        <img
+          :src="
+            `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+          "
+          alt="weather forecasting"
+        />
+      </div>
+    </template>
+
+    <template v-else>
+      <div class="col column text-center text-white">
+        <div class="col text-h2 text-weight-thin">
+          Quasar
+          <br />Wheather
+        </div>
+        <q-btn @click="getLocation" class="col" flat>
+          <q-icon left size="3em" name="my_location" />
+          <div>Find my location</div>
+        </q-btn>
+      </div>
+    </template>
+
+    <div class="col skyline"></div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageIndex",
+  data() {
+    return {
+      search: "",
+      weatherData: null,
+      lat: null,
+      lon: null,
+      apiUrl: "https://api.openweathermap.org/data/2.5/weather",
+      apiKey: "b42c812d97532b249dd0c15d6e251d41"
+    };
+  },
+  methods: {
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lon = position.coords.longitude;
+        this.getWeatherByCoords();
+      });
+    },
+    getWeatherByCoords() {
+      this.$axios(
+        `${this.apiUrl}?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
+      ).then(response => {
+        this.weatherData = response.data;
+      });
+    }
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.q-page
+  background: linear-gradient(to bottom, #136a8a, #267871)
+  .degree
+    top: -44px
+  .skyline
+    flex: 0 0 68px
+    background-image: url(../statics/skyline.png)
+    background-size: contain
+    background-position: center bottom
+</style>
